da2f49568f2f8979276ca7257ce1b2e0329c091c
YannickBihege <yannickbtheone@hotmail.com>
2022-09-06 09:43:58 +0200

    Initialize Spring Batch project

---
+++ batch-processing-records/src/main/java/com/bihege/batchprocessingrecords/BatchProcessingRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.bihege.batchprocessingrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessingRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProcessingRecordsApplication.class, args);
+	}
+
+}
---
+++ batch-processing-records/src/test/java/com/bihege/batchprocessingrecords/BatchProcessingRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bihege.batchprocessingrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
